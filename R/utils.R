841f840515a7ad060fd975f0662e74bfce60f01b
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-04-05 12:51:47 -0700

    moving break_help function

--- R/utils.R
+++ R/utils.R
@@ -47,4 +47,16 @@ para_index <- function(n, FUN, ...) {
   out
 }
 
-
+#' Repeating of indexes
+#'
+#' @param x Numerical, vector.
+#' @return Numerical.
+#' @examples
+#' break_help(c(1, 2, 3))
+#' break_help(c(6, 8, 23, 50))
+#' @export
+break_help <- function(x) {
+  unlist(
+    purrr::map2(x, 1:length(x), ~ rep(.y, .x))
+  )
+}
