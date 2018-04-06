da607a726ec5cba5ff06425abeb83e980135c6e7
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-04-06 14:11:57 -0700

    documentation and 0.2.0

--- R/nest_paragraphs.R
+++ R/nest_paragraphs.R
@@ -5,13 +5,14 @@
 #'
 #' @param data data.frame. With one paragraph per row.
 #' @param input column that gets split as string or symbol.
+#' @param ... Extra arguments passed to str_wrap.
 #' @return data.frame.
 #' @export
-nest_paragraphs <- function(data, input) {
+nest_paragraphs <- function(data, input, ...) {
   quo_input <- rlang::quo_name(rlang::enquo(input))
 
   data[[quo_input]] %>%
-    stringr::str_wrap() %>%
+    stringr::str_wrap(...) %>%
     stringr::str_split("\n") %>%
     unlist() %>%
     data.frame(text = ., stringsAsFactors = FALSE)
